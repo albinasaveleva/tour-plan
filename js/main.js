545eb4e432f94cab63a063842eff58f5b3505df7
albinasaveleva <savelevalbina@gmail.com>
2020-11-23 13:45:08 +0300

    create: styles for slider

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__button_next',
+      prevEl: '.slider__button_prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
