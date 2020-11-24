1b096c4d7d022647324913cda4761e33dd5866d0
albinasaveleva <savelevalbina@gmail.com>
2020-11-24 20:32:17 +0300

    fix: map

--- js/main.js
+++ js/main.js
@@ -18,3 +18,39 @@ var mySwiper = new Swiper('.swiper-container', {
       onlyInViewport: false,
     },
   })
+ymaps.ready(init);
+var myMap,
+    bigMap = false;
+
+function init(){
+      var myMap = new ymaps.Map("map", {
+          center: [41.01003, 28.97968],
+          zoom: 7
+      },
+      {
+        autoFitToViewport: 'always',
+        searchControlProvider: 'yandex#search'
+    });
+      myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [41.01003, 28.97968]
+        }    
+      });
+      myMap.geoObjects
+        .add(myGeoObject)
+        .add(new ymaps.Placemark([41.01003, 28.97968], {
+          iconColor: '#0095b6'
+        }));
+$('#toggler').click(toggle);
+}
+
+function toggle () {
+    bigMap = !bigMap;
+
+    if (bigMap) {
+        $('#map').removeClass('smallMap');
+    } else {
+        $('#map').addClass('smallMap');
+    }
+}
