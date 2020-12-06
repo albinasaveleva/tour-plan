2f63651d7ccbd314e29c5039d3744f7f8663ca73
albinasaveleva <savelevalbina@gmail.com>
2020-12-06 21:34:35 +0300

    create: js for modal

--- js/main.js
+++ js/main.js
@@ -1,60 +1,77 @@
-var hotelSlider = new Swiper('.hotel__swiper-container', {
+$(document).ready(function() {
+  var hotelSlider = new Swiper('.hotel__swiper-container', {
 
-  loop: true,
+    loop: true,
+        navigation: {
+        nextEl: '.hotel__swiper-button-next',
+        prevEl: '.hotel__swiper-button-prev',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+    })
+  var reviewSlider = new Swiper('.review__swiper-container', {
+  
+    loop: true,
       navigation: {
-      nextEl: '.hotel__swiper-button-next',
-      prevEl: '.hotel__swiper-button-prev',
+      nextEl: '.review__swiper-button-next',
+      prevEl: '.review__swiper-button-prev',
     },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
     },
   })
-var reviewSlider = new Swiper('.review__swiper-container', {
-
-  loop: true,
-    navigation: {
-    nextEl: '.review__swiper-button-next',
-    prevEl: '.review__swiper-button-prev',
-  },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-  },
-})
+    
+  ymaps.ready(init);
   
-ymaps.ready(init);
-
-function init(){
-      var myMap = new ymaps.Map("map", {
-          center: [41.01003, 28.97968],
-          zoom: 7
-      },
-      {
-        autoFitToViewport: 'always',
-        searchControlProvider: 'yandex#search'
-    });
-      myGeoObject = new ymaps.GeoObject({
-        geometry: {
-            type: "Point",
-            coordinates: [41.01003, 28.97968]
-        }    
+  function init(){
+        var myMap = new ymaps.Map("map", {
+            center: [41.01003, 28.97968],
+            zoom: 7
+        },
+        {
+          autoFitToViewport: 'always',
+          searchControlProvider: 'yandex#search'
       });
-      myMap.geoObjects
-        .add(myGeoObject)
-        .add(new ymaps.Placemark([41.01003, 28.97968], {
-          iconColor: '#0095b6'
-        }));
-}
+        myGeoObject = new ymaps.GeoObject({
+          geometry: {
+              type: "Point",
+              coordinates: [41.01003, 28.97968]
+          }    
+        });
+        myMap.geoObjects
+          .add(myGeoObject)
+          .add(new ymaps.Placemark([41.01003, 28.97968], {
+            iconColor: '#0095b6'
+          }));
+  }
+  
+  const mobileMenuButton = document.querySelector('.burger-menu');
+  const mainPage = document.querySelector('body');
+  
+  mobileMenuButton.addEventListener('click', function() {
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_mobile');
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_hidden');
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_scroll');
+    document.querySelector('.user-mobile').classList.toggle('user-mobile_visible');
+    document.querySelector('.form-mobile').classList.toggle('form-mobile_visible');
+    mainPage.classList.toggle('body_scrollless');
+  })
 
-const mobileMenuButton = document.querySelector('.burger-menu');
-const mainPage = document.querySelector('body');
+  const modalOpen = $("[data-toggle=modal-open]");
+  const modalClose = $("[data-toggle=modal-close]");
+  const modal = $('.modal');
+  const modalDialog = $('.modal-dialog');
 
-mobileMenuButton.addEventListener('click', function() {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_mobile');
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_hidden');
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_scroll');
-  document.querySelector('.user-mobile').classList.toggle('user-mobile_visible');
-  document.querySelector('.form-mobile').classList.toggle('form-mobile_visible');
-  mainPage.classList.toggle('body_scrollless');
-})
+  modalOpen.on('click', function() {
+    modal.addClass('modal_visibility');
+    modalDialog.addClass('modal-dialog_visibility');
+  });
+  modalClose.on('click', function() {
+    modal.removeClass('modal_visibility');
+    modalDialog.removeClass('modal-dialog_visibility');
+  });
+
+});
